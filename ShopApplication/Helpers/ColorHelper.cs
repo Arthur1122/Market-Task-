50c083940a8450d162a509c5064eb7910234c24d
art-grig <artem3.1416@gmail.com>
2019-09-23 17:21:51 +0300

    introduced dropdowns for picking colors

---
+++ ShopApplication/Helpers/ColorHelper.cs
@@ -0,0 +1,17 @@
+﻿using ShopApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApplication.Helpers
+{
+    public static class ColorHelper
+    {
+        public static List<ColorModel> ColorList = new List<ColorModel>
+        {
+            new ColorModel { Code = "Code1", Name = "Name1" },
+            new ColorModel { Code = "Code2", Name = "Name2" }
+        };
+    }
+}
